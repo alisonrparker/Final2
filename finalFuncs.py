e7cd5add972102966c17876f0910690ca4dd29a4
jbillow <jeremy.billow3@gmail.com>
2016-05-06 17:59:23 -0700

    Added files via upload

---
+++ finalFuncs.py
@@ -0,0 +1,50 @@
+class User:
+	def __init__(self, district, income, persons):
+		self.district = district
+		self.income = income
+		self.persons = persons
+
+class Cost:	
+	def __init__(self):
+		self.subsidies = ['SoMA', 'Bayview', 'Dogpatch', "South Beach", "Yerba Buena"]
+		self.medianI = {1:31860, 2:31860, 3:40180, 4:48500, 5:56820, 6: 65140, 7:73460, 8:81780}
+		
+
+
+def totalCost(cost, user):
+	total = 12000
+	for i in range (len(cost.subsidies)):
+		if user.district == cost.subsidies[i]:
+			total = total - 800		
+	
+	numPeople = user.persons
+	# print numPeople
+	medianIncome = cost.medianI[numPeople] 
+	if user.income < medianIncome:
+		total = total - 7000
+				
+	return total
+				
+
+def conditions(cost, user):
+	#neighborhoods and their solar generating potential in kWh per meter squared
+	places = {'Richmond': 4.12, 'Marina':4.59, 'North Beach':4.58, 'Parkside':4.12, 'Hayes Valley':4.61, 'SoMa':4.82, 'Midtown Terrace':4.12, 'Bayview':4.44, 'Portola':4.5, 'Visitacion Valley':4.79, 'Castro':4.41, 'Mission':4.58, 'South San Francisco': 5.04, 'Lake Shore':4.15,'South Beach':4.64, 'Dogpatch':4.65, 'Alamo Square/Western Addition':3.79, 'Sunnyside':4.23, 'Union Square':4.30, 'Financial District':4.47, 'Yerba Buena':4.45, 'Embarcadero':4.83, 'Nopa':4.28}
+	#average annual kWh consumed per capita by neighborhood
+	electricity = {'Richmond': 1945, 'Marina': 2314, 'North Beach':1634, 'Parkside':1634, 'Hayes Valley':1391, 'SoMa':2993, 'Midtown Terrace':2314, 'Bayview':1391, 'Portola':1391, 'Visitacion Valley':1391, 'Castro':2314, 'Mission':1391, 'South San Francisco': 1391, 'Lake Shore':1634, 'South Beach':1945, 'Dogpatch':1945, 'Alamo Square/Western Addition':1945, 'Sunnyside':1391, 'Union Square':1945, 'Financial District':1634, 'Yerba Buena':1945, 'Embarcadero':1391, 'Nopa':1945}
+	home = user.district
+	family = user.persons
+	solar = places[home]
+	utility = electricity[home]
+	average = 4.46
+	if solar >= average: #for neighborhoods in the upper half of solar generating potential
+		if cost <= (((utility*family)*(.1534))*10): #if the cost of install is lower than the average family utility cost over 10 years
+			ranking = 'an excellent'
+		if cost > (((utility*family)*(.1534))*10): #if the cost of install is higher than the average family utility cost over 10 years
+			ranking = 'a below average'
+	if solar < average: #for neighborhoods in the lower half of solar generating potential
+		if cost <= (((utility*family)*(.1534))*10):
+			ranking = 'an average'
+		if cost > (((utility*family)*(.1534))*10):
+			ranking = 'a poor' 
+		
+	return ranking
---
+++ final_controller.py
@@ -0,0 +1,84 @@
+# [START imports]
+import os
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+# session code from this example:
+# url: http://webapp-improved.appspot.com/api/webapp2_extras/sessions.html
+
+import jinja2
+import webapp2
+from webapp2_extras import sessions
+
+# import your mastermind functions
+import finalFuncs
+
+# boiler plate code for sessions
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+# [END imports]
+
+# more boiler plate code, just leave it as is
+class BaseHandler(webapp2.RequestHandler):  # Copied from Google's doc
+    def dispatch(self):
+        # Get a session store for this request.
+        self.session_store = sessions.get_store(request=self.request)
+
+        try:
+            # Dispatch the request.
+            webapp2.RequestHandler.dispatch(self)
+        finally:
+            # Save all sessions.
+            self.session_store.save_sessions(self.response)
+
+    @webapp2.cached_property
+    def session(self):
+        # Returns a session using the default cookie key.
+        return self.session_store.get_session()
+
+# [START main_page]
+class MainPage(BaseHandler):
+
+	def get(self):
+	
+		template_values= {}
+		template = JINJA_ENVIRONMENT.get_template('finalproject.html')
+		self.response.write(template.render(template_values))
+
+   
+# [END main_page]
+class ControllerPage(BaseHandler):
+
+	 def get(self):
+	 	district = self.request.get("district")
+	 	income= self.request.get("income")
+	 	income = int(income)
+	 	persons = self.request.get("persons")
+	 	persons = int(persons) 
+	 	user = finalFuncs.User(district, income, persons)
+	 	cost = finalFuncs.Cost()
+	 	total = finalFuncs.totalCost(cost, user)
+	 	condition = finalFuncs.conditions(total, user)
+	 	
+	 	template_values= {'Cost':total, 'Condition':condition}
+	 	
+	 	template = JINJA_ENVIRONMENT.get_template('DisplaySolarInfo.html')
+	 	self.response.write(template.render(template_values))
+
+
+# boiler plate, leave as is
+config = {}
+config['webapp2_extras.sessions'] = {
+    'secret_key': 'my-super-secret-key',
+}
+
+
+# here is where you map your url requests to handlers
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/send', ControllerPage),
+], config=config, debug=True)
